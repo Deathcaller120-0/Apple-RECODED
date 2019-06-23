7481f266a2221f7b014b85d2fb3a71b23921d353
Jon <deathcaller120@outlook.com>
2019-06-23 12:32:39 -0700

    Update js.js

--- js.js
+++ js.js
@@ -1,65 +1,216 @@
 //Player
-var PLAYER = {MON:10, APP:0, WORK:0};
+var PLAYER = {MON:0, APP:0, BAW:0, MAW:0, GAW:0, RE:0};
+var PRICE = {BAWP:35, MAWP:45, GAWP:75};
 
-//Game Objects
-var CHARACTER;
-
-//Game
-function GAMESTART(){
-	GAME.start();
-	CHARACTER = new component(30, 30, "red", 10, 120);
-}
-
-var GAME = {
-	canvas : document.createElement("canvas"),
-	start : function() {
-		this.canvas.width = 900;
-		this.canvas.height = 900;
-		this.context = this.canvas.getContext("2d");
-		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-		this.interval = setInterval(updateGameArea, 20);
-		window.addEventListener('keydown', function (e) {
-			GAME.keys = (GAME.keys || []);
-			GAME.keys[e.keyCode] = (e.type == "keydown");
-		})
-		window.addEventListener('keyup', function (e) {
-			GAME.keys[e.keyCode] = (e.type == "keydown");            
-		})
-	},
-	clear : function(){
-		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+function LOAD(){
+	PLAYER.MON = localStorage.getItem('playerMON');
+	PLAYER.APP = localStorage.getItem('playerAPP');
+	PLAYER.BAW = localStorage.getItem('playerBAW');
+	PLAYER.MAW = localStorage.getItem('playerMAW');
+	PLAYER.GAW = localStorage.getItem('playerGAW');
+	PLAYER.RE = localStorage.getItem('playerRE');
+	
+	if (PLAYER.MON == undefined){
+		PLAYER.MON = 0;
 	}
-};
-
-//Spawn Player
-function component(width, height, color, x, y) {
-    this.gamearea = GAME;
-    this.width = width;
-    this.height = height;
-    this.speedX = 0;
-    this.speedY = 0;    
-    this.x = x;
-    this.y = y;    
-    this.update = function() {
-        ctx = GAME.context;
-        ctx.fillStyle = color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
-    this.newPos = function() {
-        this.x += this.speedX;
-        this.y += this.speedY;        
-    }    
+	if (PLAYER.APP == undefined){
+		PLAYER.APP = 0;
+	}
+	if (PLAYER.BAW == undefined){
+		PLAYER.BAW = 0;
+	}
+	if (PLAYER.MAW == undefined){
+		PLAYER.MAW = 0;
+	}
+	if (PLAYER.GAW == undefined){
+		PLAYER.GAW = 0;
+	}
+	if (PLAYER.RE == undefined){
+		PLAYER.RE = 0;
+	}
+	
+	PRICE.BAWP = localStorage.getItem('priceBAW');
+	PRICE.MAWP = localStorage.getItem('priceMAW');
+	PRICE.GAWP = localStorage.getItem('priceGAW');
+	
+	if (PRICE.BAWP == undefined){
+		PRICE.BAWP = 35;
+	}
+	if (PRICE.MAWP == undefined){
+		PRICE.MAWP = 45;
+	}
+	if (PRICE.GAWP == undefined){
+		PRICE.GAWP = 75;
+	}
+	
+	if (PLAYER.BAW >= 1){
+		var ph1 = PLAYER.BAW;
+		setInterval(function(){
+			setInterval(function(){
+				PLAYER.APP++;
+			}, 10000);
+			ph1--;
+			if (ph1 <= 0){
+				clearInterval(this);
+			}
+		},1000);
+	}
+	if (PLAYER.MAW >= 1){
+		var ph2 = PLAYER.MAW;
+		setInterval(function(){
+			setInterval(function(){
+				PLAYER.APP++;
+			}, 8000);
+			ph2--;
+			if (ph2 <= 0){
+				clearInterval(this);
+			}
+		}, 1000);
+	}
+	if (PLAYER.GAW >= 1){
+		var ph3 = PLAYER.GAW;
+		setInterval(function(){
+			setInterval(function(){
+				PLAYER.APP += 2;
+			}, 6000);
+			ph3--;
+			if (ph3 <= 0){
+				clearInterval(this);
+			}
+		}, 1000);
+	}
+	
+	UPDATE();
+}
+//Update Player Status
+var NUPSPEED = setInterval(UPDATE, 1000);
+function UPDATE(){
+	var MUPD, MUPB, AUPD, ASUPD, BAW, MAW, GAW;
+	//Set things to update
+	MUPD = document.getElementById('MONEY');
+	MUPB = document.getElementById('CM');
+	AUPD = document.getElementById('APPLES');
+	ASUPD = document.getElementById('APPBAR');
+	BAWA = document.getElementById('BAWA');
+	MAWA = document.getElementById('MAWA');
+	GAWA = document.getElementById('GAWA');
+	BAW = document.getElementById('BAWBUT');
+	MAW = document.getElementById('MAWBUT');
+	GAW = document.getElementById('GAWBUT');
+	
+	//Stat Updater
+	MUPD.innerHTML = PLAYER.MON;
+	AUPD.innerHTML = PLAYER.APP;
+	BAWA.innerHTML = PLAYER.BAW;
+	MAWA.innerHTML = PLAYER.MAW;
+	GAWA.innerHTML = PLAYER.GAW;
+	
+	//Worker / Resource Updater
+	if (PLAYER.APP >= 5){
+		MUPB.style.backgroundColor = "#393";
+	} else {
+		MUPB.style.backgroundColor = "#323639";
+	}
+	if (PLAYER.MON >= PRICE.BAWP){ //Basic
+		BAW.style.backgroundColor = "#c90";
+	} else {
+		BAW.style.backgroundColor = "#323639";
+	}
+	if (PLAYER.MON >= PRICE.MAWP){ //Average
+		MAW.style.backgroundColor = "#00ffff";
+	} else {
+		MAW.style.backgroundColor = "#323639";
+	}
+	if (PLAYER.MON >= PRICE.GAWP){ //God Tier
+		GAW.style.backgroundColor = "#5900b3";
+	} else {
+		GAW.style.backgroundColor = "#323639"
+	}
+	
+	//Storage
+	AUPD = PLAYER.APP / 100000 * 100; //100,000
+	ASUPD.style.width = AUPD + "%";
+	if (AUPD >= 100){
+		PLAYER.APP = 100000;
+		ASUPD.style.backgroundColor = "#f00";
+	} else {
+		ASUPD.style.backgroundColor = "#0f0";
+	}
 }
 
-//Clear area where player IS NOT
-function updateGameArea() {
-    GAME.clear();
-    CHARACTER.speedX = 0;
-    CHARACTER.speedY = 0;    
-    if (GAME.keys && GAME.keys[37]) {CHARACTER.speedX = -1; }
-    if (GAME.keys && GAME.keys[39]) {CHARACTER.speedX = 1; }
-    if (GAME.keys && GAME.keys[38]) {CHARACTER.speedY = -1; }
-    if (GAME.keys && GAME.keys[40]) {CHARACTER.speedY = 1; }
-    CHARACTER.newPos();    
-    CHARACTER.update();
+//Saved or !Saved
+//setInterval(SAVE, 600000000);
+function SAVE(){
+	localStorage.setItem('playerAPP', PLAYER.APP);
+	localStorage.setItem('playerMON', PLAYER.MON);
+	localStorage.setItem('playerBAW', PLAYER.BAW);
+	localStorage.setItem('playerMAW', PLAYER.MAW);
+	localStorage.setItem('playerGAW', PLAYER.GAW);
+	localStorage.setItem('priceBAW', PRICE.BAWP);
+	localStorage.setItem('priceMAW', PRICE.MAWP);
+	localStorage.setItem('priceGAW', PRICE.GAWP);
+	
+	var TXT = "Saved Successfully";
+	var i = 0;
+	setTimeout(a, 20)
+	function a(){
+		if(i < TXT.length){
+			document.getElementById('SAVED').innerHTML += TXT.charAt(i);
+			i++;
+			setTimeout(a, 100);
+		} else {
+			setTimeout(function(){
+				document.getElementById('SAVED').innerHTML = " ";
+			}, 5000);
+		}
+	}
+}
+
+//Resources
+function SELLAPP(){
+	if (PLAYER.APP >= 5){
+		PLAYER.APP -= 5;
+		PLAYER.MON++;
+	}
+}
+
+//Workers
+function BAWB(){
+	if (PLAYER.MON >= PRICE.BAWP && PLAYER.BAW <= 500){
+		PLAYER.MON -= PRICE.BAWP;
+		PLAYER.BAW++;
+		setInterval(function(){
+			PLAYER.APP++;
+		}, 10000); //After 20 sec, app++;
+	}
+	if (PLAYER.BAW >= 500){
+		document.getElementById('BAWBUT').innerHTML = "MAXED OUT";
+		document.getElementById('BAWBUT').disabled = true;
+	}
+}
+function MAWB(){
+	if (PLAYER.MON >= PRICE.MAWP && PLAYER.MAW <= 500){
+		PLAYER.MON -= PRICE.MAWP;
+		PLAYER.MAW++;
+		setInterval(function(){
+			PLAYER.APP++;
+		}, 8000); //After 18 sec
+	}
+	if (PLAYER.MAW >= 500){
+		document.getElementById('MAWBUT').innerHTML = "MAXED OUT";
+		document.getElementById('MAWBUT').disabled = true;
+	}
+}
+function GAWB(){
+	if (PLAYER.MON >= PRICE.GAWP && PLAYER.GAW <= 500){
+		PLAYER.MON -= PRICE.GAWP;
+		PLAYER.GAW++;
+		setInterval(function(){
+			PLAYER.APP += 2;
+		}, 6000); //After 16 sec app +2
+	}
+	if (PLAYER.GAW >= 500){
+		document.getElementById('GAWBUT').innerHTML = "MAXED OUT";
+		document.getElementById('GAWBUT').disabled = true;
+	}
 }
