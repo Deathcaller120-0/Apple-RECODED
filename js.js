ebee7a69d2a4eb235980f3a932b77ac4665d597d
Deathcaller120-0 <49953301+Deathcaller120-0@users.noreply.github.com>
2019-10-08 08:02:53 -0700

    Update js.js

--- js.js
+++ js.js
@@ -188,7 +188,7 @@ function UPDATE(){
 	document.getElementById('APPSTAT').innerHTML = PLAYER.APP + " out of " + PLAYER.MS;
 	if (AUPD >= 95){
 		ASUPD.style.backgroundColor = "#f00";
-	} else if(AUPD >= 76){
+	} else if(AUPD >= 70){
 		ASUPD.style.backgroundColor = '#ff0';
 	} else {
 		ASUPD.style.backgroundColor = "#0f0";
